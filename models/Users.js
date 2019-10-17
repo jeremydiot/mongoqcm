fe52df049bf1d968d38eacc2357d2e895071ab12
matteomoisan22 <matteodev22@gmail.com>
2019-10-17 15:18:04 +0200

    màj du model user

--- models/Users.js
+++ models/Users.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 const usersSchema = new Schema({
 	username: {type: String, required: true},
 	email: {type: String, required: true},
-	pwd: {type: String, required: true }
+	pwd: {type: String, required: true },
+	questions: [
+		{[type: Schema.Types.ObjectId]},
+	]
 });
 
 module.exports = mongoose.model('User', usersSchema);
